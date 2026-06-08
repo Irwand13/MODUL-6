a736587c74c0b35b1221976dee95b2b1b3c5d06d
Irwanda Agus <Irwanff41@gmail.com>
2026-06-08 08:54:03 +0700

    perbaikan code dan menambahkan panduan

--- counter_race.c
+++ counter_race.c
@@ -6,16 +6,22 @@
 int counter = 0;
 
 void* worker(void* arg) {
-    for(int i = 0; i < ITER; i++) counter++; 
+    for(int i = 0; i < ITERATIONS; i++) {
+        counter++;
+    }
     return NULL;
 }
 
 int main() {
     pthread_t t1, t2;
+
     pthread_create(&t1, NULL, worker, NULL);
     pthread_create(&t2, NULL, worker, NULL);
+
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
+
     printf("FINAL = %d\n", counter);
+
     return 0;
 }
