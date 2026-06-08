a0b8448a8a67e6bf30b8e656f9a9e679a5110ba0
Irwanda Agus <Irwanff41@gmail.com>
2026-06-08 08:34:00 +0700

    First commit

---
+++ counter_race.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define ITERATIONS 100000
+
+int counter = 0;
+
+void* worker(void* arg) {
+    for(int i = 0; i < ITER; i++) counter++; 
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, worker, NULL);
+    pthread_create(&t2, NULL, worker, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    printf("FINAL = %d\n", counter);
+    return 0;
+}
